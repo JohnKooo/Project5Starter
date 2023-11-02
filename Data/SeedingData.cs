d5690aeb5da3c027ee559d7f16e905ee626a9ab9
JohnKooo <johnkok7534@gmail.com>
2023-11-02 11:28:14 -0500

    1st Change

---
+++ Data/SeedingData.cs
@@ -0,0 +1,63 @@
+﻿using CourseCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseCatalog.Data
+{
+    public class SeedingData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new CourseCatalogContext(
+                serviceProvider.GetRequiredService<DbContextOptions<CourseCatalogContext>>()))
+            {
+                if (context == null || context.Course == null)
+                {
+                    throw new ArgumentNullException("Null Context");
+                }
+                if (context.Course.Any())
+                {
+                    return;
+                }
+                context.Course.AddRange(
+                    new Course()
+                    {
+                        Id = 101010,
+                        CourseName = "CIS 103",
+                        CourseDescription = "Class",
+                        RoomNumber = 169,
+                    },
+                    new Course()
+                    {
+                        Id = 101011,
+                        CourseName = "CIS 103",
+                        CourseDescription = "Class",
+                        RoomNumber = 169,
+                        
+                    },
+                    new Course()
+                    {
+                        Id = 101012,
+                        CourseName = "CIS 103",
+                        CourseDescription = "Class",
+                        RoomNumber = 169,
+                    },
+                    new Course()
+                    {
+                        Id = 101013,
+                        CourseName = "CIS 103",
+                        CourseDescription = "Class",
+                        RoomNumber = 169,
+                    },
+                    new Course()
+                    {
+                        Id = 101014,
+                        CourseName = "CIS 103",
+                        CourseDescription = "Class",
+                        RoomNumber = 169,
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
